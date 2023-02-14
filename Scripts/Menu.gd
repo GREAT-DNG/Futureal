8c4eb79eb3eb1f81437e25a72aaa0da9e0dd2a2f
GREAT DNG <greatdng@gmail.com>
2023-02-14 17:12:35 +0700

    Fixed settings saving

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -7,9 +7,15 @@ var settings_saver = load("res://Scripts/SettingsSaver.gd").new()
 func _ready():
 	if settings_saver.is_settings_exsists():
 		$SettingsPanel/FullscreenCheckButton.pressed = settings_saver.get_fullscreen_state()
+		OS.window_fullscreen = $SettingsPanel/FullscreenCheckButton.pressed
+		if(!$SettingsPanel/FullscreenCheckButton.pressed):
+			OS.window_size = Vector2(800, 600)
+			
 		$SettingsPanel/MuteCheckButton.pressed = settings_saver.get_mute_state()
+		AudioServer.set_bus_mute(0, $SettingsPanel/MuteCheckButton.pressed)
+		
 		$SettingsPanel/AutoreloadCheckButton.pressed = settings_saver.get_autoreload_state()
-
+	
 # warning-ignore:unused_argument
 func _input(event):
 	if Input.is_key_pressed(KEY_F1):
@@ -77,16 +83,15 @@ func _on_FileDialog_file_selected(path):
 		# warning-ignore:return_value_discarded
 		get_tree().change_scene_to(load("res://Scenes/Menu.tscn"))
 	
-func _on_FullscreenCheckButton_toggled(button_pressed):
-	OS.window_fullscreen = button_pressed
-	if(!button_pressed):
+func _on_FullscreenCheckButton_button_up():
+	OS.window_fullscreen = $SettingsPanel/FullscreenCheckButton.pressed
+	if(!$SettingsPanel/FullscreenCheckButton.pressed):
 		OS.window_size = Vector2(800, 600)
 	save_settings()
 	
-func _on_MuteCheckButton_toggled(button_pressed):
-	AudioServer.set_bus_mute(0, button_pressed)
+func _on_MuteCheckButton_button_up():
+	AudioServer.set_bus_mute(0, $SettingsPanel/MuteCheckButton.pressed)
 	save_settings()
 	
-# warning-ignore:unused_argument
-func _on_AutoreloadCheckButton_toggled(button_pressed):
+func _on_AutoreloadCheckButton_button_up():
 	save_settings()
