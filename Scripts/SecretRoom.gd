6fb2d4a0577122547b119b6d7449818a215acb51
GREAT DNG <greatdng@gmail.com>
2023-03-17 10:58:17 +0700

    Coins disabled, minor corrections

--- Scripts/SecretRoom.gd
+++ Scripts/SecretRoom.gd
@@ -1,5 +1,15 @@
 extends Node
 
+var is_used = false
+
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
+		if !is_used:
+			$"../Player/UI/MessageLabel".show_message("Secret found!", 3)
+		
+		is_used = true
 		$HideSprite.hide()
+	
+func _on_Area2D_body_exited(body):
+	if body.name == "Player":
+		$HideSprite.show()
